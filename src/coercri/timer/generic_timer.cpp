d03c0753adc12d7eaf3fc3e2f1b71190aeb2fef8
Stephen Thompson <stephen@solarflare.org.uk>
2012-01-06 16:48:50 +0000

    update copyright years 2011 -> 2012
    git-svn-id: svn://www.knightsgame.org.uk/trunk@62 00979ad9-a3a6-4f7a-b49c-25ba5319b333

--- src/coercri/timer/generic_timer.cpp
+++ src/coercri/timer/generic_timer.cpp
@@ -9,7 +9,7 @@
  *   21-Oct-2011
  *   
  * COPYRIGHT:
- *   Copyright (C) Stephen Thompson, 2008 - 2011.
+ *   Copyright (C) Stephen Thompson, 2008 - 2012.
  *
  *   This file is part of the "Coercri" software library. Usage of "Coercri"
  *   is permitted under the terms of the Boost Software License, Version 1.0, 
