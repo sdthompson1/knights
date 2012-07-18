ef577c006c0240909a8d045c5eeeaf97a85e0545
Stephen Thompson <stephen@solarflare.org.uk>
2012-07-18 09:47:39 +0000

    #94: slight reformatting.
    git-svn-id: svn://www.knightsgame.org.uk/trunk@173 00979ad9-a3a6-4f7a-b49c-25ba5319b333

--- knights_data/classic/items.lua
+++ knights_data/classic/items.lua
@@ -218,13 +218,16 @@ basic_wand = {
     name = "A wand"
 }
 
-i_wand_of_destruction = kts.ItemType(basic_wand & {
+i_wand_of_destruction = kts.ItemType(
+  basic_wand & {
     melee_damage = 1000,
     melee_stun_time = rng_time_range(2, 3),
     melee_tile_damage = 1000
-})
+  }
+)
 
-i_wand_of_undeath = kts.ItemType(basic_wand & {
+i_wand_of_undeath = kts.ItemType(
+  basic_wand & {
     melee_damage = 0,
     melee_stun_time = ts,
     melee_action = function()
@@ -235,9 +238,11 @@ i_wand_of_undeath = kts.ItemType(basic_wand & {
           snd_zombie()
        end
     end
-})
+  }
+)
 
-i_wand_of_open_ways = kts.ItemType(basic_wand & {
+i_wand_of_open_ways = kts.ItemType(
+  basic_wand & {
     melee_damage = rng_range(1, 3),
     melee_stun_time = rng_time_range(2, 5),
     melee_action = function()
@@ -245,9 +250,11 @@ i_wand_of_open_ways = kts.ItemType(basic_wand & {
        wandzap()
     end,
     allow_strength = false  -- Prevent the usual destruction of doors/chests when knight has strength
-})
+  }
+)
 
-i_wand_of_securing = kts.ItemType(basic_wand & {
+i_wand_of_securing = kts.ItemType(
+  basic_wand & {
     melee_damage = function()
        if kts.RandomChance(0.25) then return 0 else return 1 end
     end,
@@ -256,7 +263,8 @@ i_wand_of_securing = kts.ItemType(basic_wand & {
        local success = kts.Secure(t_wall_normal) 
        if success then wandzap() end
     end
-})
+  }
+)
 
 all_wands = { i_wand_of_destruction, i_wand_of_undeath, i_wand_of_open_ways, i_wand_of_securing }
 
@@ -278,7 +286,8 @@ basic_book = {
 
 i_basic_book = kts.ItemType(basic_book)
 
-i_book_of_knowledge = kts.ItemType(basic_book & {
+i_book_of_knowledge = kts.ItemType(
+  basic_book & {
     -- Ancient Book of Knowledge
     on_pick_up = function()
         kts.MagicMapping()
@@ -290,9 +299,11 @@ i_book_of_knowledge = kts.ItemType(basic_book & {
         kts.UpdateQuestStatus()
     end,
     editor_label = "K"
-})
+  }
+)
 
-i_necronomicon = kts.ItemType(basic_book & { 
+i_necronomicon = kts.ItemType(
+  basic_book & { 
     -- Necronomicon
     -- Note on kts.Necromancy: first number is the number of zombies to generate (I have increased
     -- this slightly compared to the original Knights), and second number is the range (in squares). 
@@ -307,7 +318,8 @@ i_necronomicon = kts.ItemType(basic_book & {
         kts.UpdateQuestStatus()
     end,
     editor_label = "N"
-})
+  }
+)
 
 all_books = { i_basic_book, i_book_of_knowledge, i_necronomicon }
 
@@ -397,25 +409,32 @@ basic_key = {
     tutorial = TUT_KEY
 }
 
-i_key1 = kts.ItemType (basic_key & {
+i_key1 = kts.ItemType (
+  basic_key & {
     backpack_graphic = g_inv_key1,
     backpack_slot = 20,
     key = 1
-})
+  }
+)
 
-i_key2 = kts.ItemType(basic_key & {
+i_key2 = kts.ItemType(
+  basic_key & {
     backpack_graphic = g_inv_key2,
     backpack_slot = 21,
     key = 2
-})
+  }
+)
 
-i_key3 = kts.ItemType(basic_key & {
+i_key3 = kts.ItemType(
+  basic_key & {
     backpack_graphic = g_inv_key3,
     backpack_slot = 22,
     key = 3
-})
+  }
+)
 
-i_lockpicks = kts.ItemType(basic_key & {
+i_lockpicks = kts.ItemType(
+  basic_key & {
     backpack_graphic = g_inv_lockpicks,
     backpack_slot = 23,
     key = -1,
@@ -431,7 +450,8 @@ i_lockpicks = kts.ItemType(basic_key & {
        name = "Use Lock Picks"
     },
     tutorial = TUT_LOCKPICKS
-})
+  }
+)
 
 
 --
